4226b2a553f9012f31e4bdef9fb1e3d4672173d5
Maxim Ralovec <m.ralovec@gmail.com>
2018-07-07 01:07:46 +0200

    [refactoring] raml update *version 2.0.2 *api/impl change

--- raml-example-impl/src/main/java/com/raml/example/impl/TodoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.raml.example.impl;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "no todo found with given id")
-public class TodoNotFoundException extends RuntimeException {
-}
